a27cebd254a0205285750237a4160e37e753ec91
Nemanja Malesija <malesija13@gmail.com>
2023-02-01 00:41:49 +0100

    Implement search feature, fix movie description hover transition

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,29 @@ import MovieCard from './components/MovieCard';
 function App() {
   const queryClient = useQueryClient();
   const [searchTerm, setSearchTerm] = useState('batman');
+  const [searchTermDummy, setSearchTermDummy] = useState('Batman');
 
   const { data, isLoading } = useQuery(['movies', searchTerm], fetchMovies);
 
   if (isLoading) return <h1>Loading...</h1>;
   console.log(data);
 
+  const submitHandler = () => {
+    setSearchTerm(searchTermDummy);
+  };
+
   return (
     <main>
+      <form onSubmit={submitHandler}>
+        <div className="form-control">
+          <label className="main-label">Search for a movie</label>
+          <input
+            type="text"
+            placeholder={searchTermDummy}
+            onChange={(e) => setSearchTermDummy(e.target.value)}
+          />
+        </div>
+      </form>
       <div className="movies-container">
         {data?.map((movie) => {
           return <MovieCard key={crypto.randomUUID()} {...movie} />;
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -4,9 +4,15 @@ import { movieCard } from '../constants/types';
 const MovieCard = (props: movieCard) => {
   const { Poster, Title, Type, Year, imdbID } = props;
   return (
-    <article className='fetched-movie'>
+    <article className="movie-card">
       <img src={Poster} alt={Title} />
-      <div className="movie-description">
+      <div
+        className={
+          Title.length >= 30
+            ? 'movie-description movie-description-translateExtra'
+            : 'movie-description'
+        }
+      >
         <h5 className="heading-fifth">{Title}</h5>
         <p className="movie-year">{Year}</p>
       </div>
